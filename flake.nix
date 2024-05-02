ca3f12e142055c78edc2ca36a190378437d6df04
Stephane Fillon <stephane.fillon@gmail.com>
2024-05-03 00:45:36 +0700

    fix flake

--- flake.nix
+++ flake.nix
@@ -1,15 +1,11 @@
 {
   description = "A very basic flake";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
-  };
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in { packages.${system}.default = pkgs.callPackage ./genymotion.nix { }; };
 }
